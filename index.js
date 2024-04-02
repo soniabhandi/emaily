a06fd6bfa93015f2378d7a7f269019ba5a0d4c01
soniabhandi <soniabhandi@gmail.com>
2024-04-02 13:14:32 +0530

    added dotenv

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-require(dotenv).config();
+require("dotenv").config();
 const cookieSession = require("cookie-session");
 const passport = require("passport");
 const keys = require("./config/keys");
