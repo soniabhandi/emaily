2df78cbe54a2a1e6c3b4e0257bae079891fc1e90
soniabhandi <soniabhandi@gmail.com>
2024-04-02 20:47:02 +0530

    removed github strategy

--- services/passport.js
+++ services/passport.js
@@ -1,7 +1,7 @@
 const passport = require("passport");
 const mongoose = require("mongoose");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const GithubStrategy = require("passport-github").Strategy;
+// const GithubStrategy = require("passport-github").Strategy;
 const keys = require("../config/keys");
 
 const User = mongoose.model("users");
@@ -39,24 +39,24 @@ passport.use(
   )
 );
 
-passport.use(
-  new GithubStrategy(
-    {
-      clientID: keys.githubClientID,
-      clientSecret: keys.githubClientSecret,
-      callbackURL: "/auth/github/callback",
-    },
-    (refreshToken, accessToken, profile, done) => {
-      User.findOne({ githubId: profile.id }).then((existingUser) => {
-        if (existingUser) {
-          done(null, existingUser);
-        } else {
-          new User({ githubId: profile.id })
-            .save()
-            .then((user) => done(null, user));
-        }
-      });
-      // console.log(profile);
-    }
-  )
-);
+// passport.use(
+//   new GithubStrategy(
+//     {
+//       clientID: keys.githubClientID,
+//       clientSecret: keys.githubClientSecret,
+//       callbackURL: "/auth/github/callback",
+//     },
+//     (refreshToken, accessToken, profile, done) => {
+//       User.findOne({ githubId: profile.id }).then((existingUser) => {
+//         if (existingUser) {
+//           done(null, existingUser);
+//         } else {
+//           new User({ githubId: profile.id })
+//             .save()
+//             .then((user) => done(null, user));
+//         }
+//       });
+//       // console.log(profile);
+//     }
+//   )
+// );
